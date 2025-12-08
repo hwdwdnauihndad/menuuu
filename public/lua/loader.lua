ea319ed312c843baafa1283a06341234a2e1c317
bnzshi803-ctrl <bnzshi803@gmail.com>
2025-12-08 21:37:04 +0100

    Update LibraryURL to ClientLoaderURL

--- public/lua/loader.lua
+++ public/lua/loader.lua
@@ -1,4 +1,4 @@
-local LibraryURL = "https://bnzshi803-ctrl.github.io/menuuu/public/lua/library.lua"
+https://bnzshi803-ctrl.github.io/menuuu/public/lua/client_loader.lua
 local status, ClientLoaderCode = Susano.HttpGet(ClientLoaderURL)
 
 if status ~= 200 then
