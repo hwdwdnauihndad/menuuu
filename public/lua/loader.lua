f2af114652e35e3f4143c5fb294c61dee1d146d3
bnzshi803-ctrl <bnzshi803@gmail.com>
2025-12-08 21:40:51 +0100

    Update ClientLoaderURL to use raw GitHub link

--- public/lua/loader.lua
+++ public/lua/loader.lua
@@ -1,4 +1,4 @@
-local ClientLoaderURL = "https://bnzshi803-ctrl.github.io/menuuu/public/lua/client_loader.lua"
+local ClientLoaderURL = "https://raw.githubusercontent.com/bnzshi803-ctrl/menuuu/main/public/lua/client_loader.lua"
 local status, ClientLoaderCode = Susano.HttpGet(ClientLoaderURL)
 
 if status ~= 200 then
